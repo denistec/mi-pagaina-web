e3ca7d3b05d6b2013b5d6ff793430d3519e775a8
denistec <yodygranadosrojas@hotmail.com>
2025-04-08 10:14:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+// document.getElementById("boton-enviar").addEventListener('click', function(){
+//     const input = document.getElementById("ingreso-texto");
+//     const texto = input.value.trim();
+//     if (texto){
+//         const nuevoParrafo = document.createElement('p');
+//         nuevoParrafo.textContent = texto;
+//         document.getElementById("saludo").appendChild(nuevoParrafo);
+//     }
+// })
+
+//CORRECCIÓN:
+
+document.getElementById("boton-enviar").addEventListener("click", function() {
+    const input = document.getElementById("ingreso-texto");
+    const texto = input.value.trim();
+    if (texto) {
+        const saludo = `¡Hola, ${texto}!`;
+        document.getElementById("saludo").innerHTML = saludo;
+        input.value = "";
+    } else {
+        document.getElementById("saludo").innerHTML = "Ingresa tu nombre";
+    }
+});
