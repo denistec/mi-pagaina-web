1cc7c41684dbf169d41d03ad148ae7aff4bbe553
denistec <yodygranadosrojas@hotmail.com>
2025-04-08 10:48:43 -0500

    corrección js

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ document.getElementById("boton-enviar").addEventListener("click", function() {
     const input = document.getElementById("ingreso-texto");
     const texto = input.value.trim();
     if (texto) {
-        const saludo = `¡Hola, ${texto}!`;
+        const saludo = `Hola: ${texto}`;
         document.getElementById("saludo").innerHTML = saludo;
         input.value = "";
     } else {
-        document.getElementById("saludo").innerHTML = "Ingresa tu nombre";
+        document.getElementById(saludo).innerHTML = "Ingresa tu nombre";
     }
 });
