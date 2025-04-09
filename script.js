b8ecc2f801545ce1092972db9660485a2c036134
denistec <yodygranadosrojas@hotmail.com>
2025-04-08 19:48:14 -0500

    dejé mi versión y comenté la versión de un compañero

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 
 
-// document.getElementById("boton-enviar").addEventListener('click', function(){
-//     const input = document.getElementById("ingreso-texto");
-//     const texto = input.value.trim();
-//     if (texto){
-//         const nuevoParrafo = document.createElement('p');
-//         nuevoParrafo.textContent = texto;
-//         document.getElementById("saludo").appendChild(nuevoParrafo);
-//     }
-// })
-
-//CORRECCIÓN:
-
-document.getElementById("boton-enviar").addEventListener("click", function() {
+document.getElementById("boton-enviar").addEventListener('click', function(){
     const input = document.getElementById("ingreso-texto");
     const texto = input.value.trim();
-    if (texto) {
-        const saludo = `Hola: ${texto}`;
-        document.getElementById("saludo").innerHTML = saludo;
-        input.value = "";
-    } else {
-        document.getElementById(saludo).innerHTML = "Ingresa tu nombre";
+    if (texto){
+        const nuevoParrafo = document.createElement('p');
+        nuevoParrafo.textContent = texto;
+        document.getElementById("saludo").appendChild(nuevoParrafo);
     }
-});
+})
+
+//OPCIÓN COMPAÑERA:
+
+// document.getElementById("boton-enviar").addEventListener("click", function() {
+//     const input = document.getElementById("ingreso-texto");
+//     const texto = input.value.trim();
+//     if (texto) {
+//         const saludo = `Hola: ${texto}`;
+//         document.getElementById("saludo").innerHTML = saludo;
+//         input.value = "";
+//     } else {
+//         document.getElementById(saludo).innerHTML = "Ingresa tu nombre";
+//     }
+// });
